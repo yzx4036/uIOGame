f968ffcc03444c79c18071ba1b7d232a6925c88b
jeremyLee0512 <2410250284@qq.com>
2020-08-08 00:15:50 +0800

    1d动作游戏

---
+++ Source/uIO/Public/IOPlayerState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "IOPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UIO_API AIOPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+};
